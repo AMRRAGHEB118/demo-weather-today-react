36d9dc0c3b9b02b312f8f8538fa6f796a694e23a
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-08-26 16:18:20 +0300

    add translation in app

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,14 @@
 import { Typography } from '@mui/material';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function City() {
-  return <Typography variant="h2">القاهرة</Typography>;
+  const { t, i18n } = useTranslation();
+
+  useEffect(()=>{
+    i18n.changeLanguage('ar')
+  },[])
+  return <Typography variant="h2">{t('city')}</Typography>;
 }
 
 export default City;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import config from './config';
 import axios from 'axios';
+import './i18n';
 
 axios.defaults.baseURL = config.baseUrl;
 
